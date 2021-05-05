df1f619b26f1229c64ea3df659e1843fb182ca7b
Mark Miroliubov <m_mirol@inbox.ru>
2021-05-05 10:39:59 +0300

    First commit

---
+++ MagneticField/MagneticFieldTest.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "MagneticFieldStable.h"
+
+using namespace Tools;
+using namespace MagneticField;
+
+BOOL IntegrateRingOfCurrent_Test()
+{
+    F64 f64R = 0.025;      /* m */
+    F64 f64Current = 1;    /* A */
+    U64 u64NVertexes = 360;
+
+    CPoint3D RingCentrePoint(0.0, 0.0, 0.0);
+    CPoint3D InvestigationPoint(0.0, 0.0, 0.0);
+
+    CVector3D B;
+
+    CIntegration::IntegrateRingOfCurrent(RingCentrePoint, f64R, f64Current, InvestigationPoint, u64NVertexes, &B);
+    printf("B = ");
+    B.Print();
+
+    return TRUE;
+}
+
+int main()
+{
+    printf("========= MagneticFieldTest =========\n\n");
+
+    IntegrateRingOfCurrent_Test();
+
+    return 0;
+}
